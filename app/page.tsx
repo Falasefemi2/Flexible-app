fc08edb3f4a5018e879f39c6b2d082ed72e62d13
Falase Femi <femifalase228@gmail.com>
2023-07-09 14:31:29 +0100

    Navbar ongoing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="flex-start flex-col paddings mb-16">
+            {/* categories */}
+            <h1>Categories</h1>
+            {/* Posts */}
+            <h1>Posts</h1>
+            {/* LoadMore */}
+            <h1>LoadMore</h1>
+        </section>
     )
 }
 
