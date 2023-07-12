8e94e2a671c6793f4347c378d4364987498d7877
Falase Femi <femifalase228@gmail.com>
2023-07-12 02:01:11 +0100

    Google auth implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,37 +2,47 @@ import { NavLinks } from "@constants"
 import Image from "next/image"
 import Link from "next/link"
 import AuthProvider from "./AuthProvider"
+import { getCurrentUser } from "@lib/session"
 
-const Navbar = () => {
-    const session = null;
+const Navbar = async () => {
+    const session = await getCurrentUser();
+
     return (
         <nav className="flexBetween navbar">
             <div className="flex-1 flexStart gap-10">
                 <Link href="/">
                     <Image src="/logo.svg" width={115} height={43} alt="flexible" />
                 </Link>
-                <ul className="xl:flex hidden text-small gap-7"> 
-                {NavLinks.map((link) => (
-                    <Link href={link.href} key={link.key}>
-                        {link.text}
-                    </Link>
-                ))}
+                <ul className="xl:flex hidden text-small gap-7">
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
                 </ul>
             </div>
 
             <div className="flexCenter gap-4">
-                {session ? (
+                {session?.user ? (
                     <>
-                    UserPhoto
+                        {session?.user?.image &&
+                            <Image
+                                src={session.user.image}
+                                width={40}
+                                height={40}
+                                className="rounded-full"
+                                alt={session.user.name}
+                            />
+                        }
 
-                    <Link href="/create-project">
-                        Share Work
-                    </Link>
+                        <Link href="/create-project">
+                            Share Work
+                        </Link>
                     </>
                 ) : (
                     <AuthProvider />
                 )
-            }
+                }
             </div>
         </nav>
     )
